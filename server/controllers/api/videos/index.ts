620bc5de7d1022ae438ef31fa941a68ad0698291
clementbrizard <clementbrizard53@gmail.com>
2018-12-05 11:53:00 +0000

    Fix schedule case

--- server/controllers/api/videos/index.ts
+++ server/controllers/api/videos/index.ts
@@ -325,15 +325,21 @@ async function updateVideo (req: express.Request, res: express.Response) {
       if (videoInfoToUpdate.support !== undefined) videoInstance.set('support', videoInfoToUpdate.support)
       if (videoInfoToUpdate.description !== undefined) videoInstance.set('description', videoInfoToUpdate.description)
       if (videoInfoToUpdate.commentsEnabled !== undefined) videoInstance.set('commentsEnabled', videoInfoToUpdate.commentsEnabled)
-      if (videoInfoToUpdate.originallyPublishedAt !== undefined && videoInfoToUpdate.originallyPublishedAt !== null) {
-        videoInstance.set('originallyPublishedAt', videoInfoToUpdate.originallyPublishedAt)
+      if (videoInfoToUpdate.originallyPublishedAt !== undefined) {
+        if (videoInfoToUpdate.originallyPublishedAt !== null) {
+          videoInstance.set('originallyPublishedAt', videoInfoToUpdate.originallyPublishedAt)
+        }
       }
       if (videoInfoToUpdate.privacy !== undefined) {
         const newPrivacy = parseInt(videoInfoToUpdate.privacy.toString(), 10)
         videoInstance.set('privacy', newPrivacy)
 
         if (wasPrivateVideo === true && newPrivacy !== VideoPrivacy.PRIVATE) {
-          videoInstance.set('publishedAt', new Date())
+          const publicationDate = new Date()
+          videoInstance.set('publishedAt', publicationDate)
+          if (videoInfoToUpdate.originallyPublishedAt === null) {
+            videoInstance.set('originallyPublishedAt', publicationDate)
+          }
         }
       }
 
